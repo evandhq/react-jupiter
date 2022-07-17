9c52994a42eda999c47d7e510789219994d38802
mehr4nshiri <mehranshiri1996@gmail.com>
2022-07-17 13:18:24 +0430

    feat: add progress bar component

--- src/components/progress-bar/index.stories.js
+++ src/components/progress-bar/index.stories.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
-import ProgressBar from './index';
+import ProgressBar from '.';
 
 storiesOf('Progress Bar', module)
 .add('Progress Bar', () => <ProgressBar width="25%" />)
