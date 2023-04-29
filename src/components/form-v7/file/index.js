e1c2d87e9630ece4153db556e7cfcf8ee28d8616
mehr4nshiri <mehranshiri1996@gmail.com>
2023-04-29 16:23:20 +0330

    feat: add file input for react-hook V7

---
+++ src/components/form-v7/file/index.js
@@ -0,0 +1,81 @@
+// @flow
+
+import React, { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from '../../globalStyle';
+import theme from '../theme';
+import ErrorMsg from '../errorMsg';
+import {
+  Input,
+  LabelContainer,
+  DescriptionContainer,
+  PasswordIcon,
+  Label,
+} from './index.style';
+
+type Props = {
+  label?: string,
+  htmlElementName: string,
+  id?: string,
+  disabled?: boolean,
+  description?: string,
+  errorMessage?: string,
+  register?: any,
+  required?: boolean | String,
+}
+
+const FileInput = (props: Props) => {
+  const {
+    type,
+    label,
+    htmlElementName,
+    id,
+    disabled,
+    description,
+    errorMessage,
+    register,
+    required,
+  } = props;
+
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Label htmlFor={id || `${type}-${htmlElementName.split(' ').join('')}`} data-test="file-input">
+        {label && (
+          <LabelContainer bold size={14}>
+            {label}
+          </LabelContainer>
+        )}
+        {description && (
+          <DescriptionContainer bold size={10} color="gray" data-test="file-input-description">
+            {description}
+          </DescriptionContainer>
+        )}
+        <Input
+          id={id || `${type}-${htmlElementName.split(' ').join('')}`}
+          name={htmlElementName}
+          type={'file'}
+          {...register(htmlElementName, {
+            disabled,
+            required: !!required ? 'این فیلد الزامی است' : false
+          })}
+        />
+      </Label>
+      <ErrorMsg errorMessage={errorMessage} />
+    </ThemeProvider>
+  );
+};
+
+FileInput.defaultProps = {
+  type: 'text',
+  label: null,
+  id: null,
+  placeholder: null,
+  disabled: false,
+  description: null,
+  errorMessage: '',
+  register: null,
+  required: false,
+};
+
+export default FileInput;
---
+++ src/components/form-v7/file/index.stories.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { storiesOf } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import {
+  withKnobs,
+  select,
+  text,
+  boolean,
+} from '@storybook/addon-knobs';
+import FileInput from './index';
+
+storiesOf('Form-V7/Inputs', module)
+  .add('File Input', () => {
+    const {
+      register,
+    } = useForm();
+    return (
+      <FileInput
+        htmlElementName="text-name-02"
+        register={register}
+        label={text('optional label', 'این لیبل است')}
+        placeholder={text('optional placeholder', 'متن پیشفرض')}
+        disabled={boolean('disabled(default : false)', false)}
+        description={text('optional description', 'توضیحات اضافه')}
+        errorMessage={text('optional error msg', 'ارور زیبا')}
+      />
+    )
+  })
+  .addDecorator(withInfo)
+  .addDecorator(withKnobs);
